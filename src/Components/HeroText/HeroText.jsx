6a2c241d4acb4f34b7b6147d351001526f29e85c
RelaxItsAryan <aryanarya5507@gmail.com>
2026-01-14 16:03:04 +0530

    Added hero text component, small fade in animation, and proper text alignment

--- src/Components/HeroText/HeroText.jsx
+++ src/Components/HeroText/HeroText.jsx
@@ -9,7 +9,13 @@ export default function HeroText() {
           <span className="hero__we">we are</span>{' '}
           <span className="hero__gdgc">gdgc</span>
         </h1>
-        <p className="hero__sub"></p>
+        <p className="hero__sub">VIT BHOPAL</p>
+      </div>
+
+      {/* Bottom-left location (small, unobtrusive) */}
+      <div className="hero__location" aria-hidden="true">
+        <div className="hero__location-name">VIT Bhopal, India</div>
+        <div className="hero__location-coords">17.3616° N, 78.4747° E</div>
       </div>
     </section>
   )
